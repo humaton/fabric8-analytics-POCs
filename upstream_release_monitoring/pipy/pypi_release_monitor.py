315ca65f12c1086c54538b4ab7551ce64bc0ba5b
animuk <animukhe@redhat.com>
2018-09-18 15:29:31 +0530

    pipy package release check
    This script retrieves the release version of the python packages. Need to specify the python package name in the commend line argument

---
+++ upstream_release_monitoring/pipy/pypi_release_monitor.py
@@ -0,0 +1,35 @@
+# Python script to retrieve the latest release version information for python packages
+# usage example: python3 pypi_release_monitor.py aactivator
+
+import requests
+import sys
+import json
+from json.decoder import JSONDecodeError
+from requests.exceptions import HTTPError, Timeout
+
+def main():
+    try:
+        # Use requests to issue a standard HTTP GET request
+        url = "https://pypi.org/pypi/"+sys.argv[1]+"/json"  
+        result = requests.get(url, timeout=2)
+        result.raise_for_status()
+        printResults(result)
+    except HTTPError as err:
+        print("Error: {0}".format(err))
+    except Timeout as err:
+        print("Request timed out: {0}".format(err))
+    
+
+def printResults(resData):
+     # Use the built-in JSON function to return parsed data
+    try:
+        dataobj = resData.json()
+        print(dataobj['info']['version'])
+    except JSONDecodeError as err:
+        print("Whoops, JSON Decoding error:")
+        print(err.msg)
+        print(err.lineno, err.colno)
+    
+
+if __name__ == "__main__":
+    main()
