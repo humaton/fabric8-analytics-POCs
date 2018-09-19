eea9ee3da9d71b447c49ab6675d3e95da2ea9f72
Tomáš Hrčka <thrcka@redhat.com>
2018-09-19 11:38:41 +0200

    Minor tweaks

--- upstream_release_monitoring/npm/app.js
+++ upstream_release_monitoring/npm/app.js
@@ -1,13 +1,29 @@
 const ChangesStream = require('changes-stream');
 const Request = require('request');
 const Normalize = require('normalize-registry-metadata');
-
+const fs = require('fs');
 const db = 'https://replicate.npmjs.com';
-
+const latest_fetched = "/tmp/latest_fetched_npm_package";
 var changes = new ChangesStream({
   db: db,
   include_docs: true
 });
+
+var package_versions = {
+    name: '',
+    changed: ''
+};
+
+//Check if we have stored latest fetched change
+//If true fetch from it till for ever
+if (fs.existsSync(latest_fetched)) {
+  fs.readFileSync(latest_fetched, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+  console.log(data);});
+}
+
 
 Request.get(db, function(err, req, body) {
   var end_sequence = JSON.parse(body).update_seq;
@@ -15,9 +31,17 @@ Request.get(db, function(err, req, body) {
     //if (change.seq >= end_sequence) {
     //  process.exit(0);
     //}
+
     if (change.doc.name) {
       normalized = Normalize(change.doc);
-      console.log({ "name" : normalized.name, "changed": normalized.time});
+      package_versions['name'] = normalized.name;
+      package_versions['changed'] = normalized.time;
+      fs.writeFileSync(latest_fetched, JSON.stringify(package_versions), function(err) {
+        if(err) {
+          return console.log(err);
+        }
+      });
+      console.log(package_versions);
     }
   });
 });
