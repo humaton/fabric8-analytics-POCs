df2bcad7b4ed07b3631c0a674e5ffebaa654dbf8
Tomáš Hrčka <thrcka@redhat.com>
2018-09-25 10:09:20 +0200

    Monitor rewrite

--- upstream_release_monitoring/release_monitoring.py
+++ upstream_release_monitoring/release_monitoring.py
@@ -1,66 +1,84 @@
 #!/usr/bin/env python3
 import json
-import os
-import requests
+import logging
+
 import feedparser
-from selinon import run_flow
+import requests
 from f8a_worker.setup_celery import init_celery, init_selinon
+from selinon import run_flow
 
-NPM_URL = "https://registry.npmjs.org/"
-PYPI_URL = "https://pypi.org/"
-
-feed_pypi = feedparser.parse(PYPI_URL+"rss/updates.xml")
-feed_npm = feedparser.parse(NPM_URL + "-/rss")
-
-def highest_number_of_entries():
-    number_of_entries_pypi = len(feed_pypi.entries)
-    number_of_entries_npm = len(feed_npm.entries)
-    if (number_of_entries_npm > number_of_entries_pypi):
-        return number_of_entries_npm
-    elif (number_of_entries_npm < number_of_entries_pypi):
-        return number_of_entries_pypi
-    else:
-        return number_of_entries_npm
+from defaults import NPM_URL, PYPI_URL, ENABLE_SCHEDULING, \
+    SCHEDULED_NPM_PACKAGES, SCHEDULED_PYPI_PACKAGES
 
 
-def schedule_package_analisys(self, name, ecosystem, version):
-    """Run Selinon flow for analyses.
-     :param name: name of the package to analyse
-     :param version: package version
-     :param ecosystem: package ecosystem
-     :return: dispatcher ID serving flow
-     """
+class ReleaseMnitor():
+    """Class which check rss feeds for new releases"""
 
-    node_args = {
-        'ecosystem': ecosystem,
-        'name': name,
-        'version': version,
-        'recursive_limit': 0
-    }
+    def __init__(self):
+        self.log = logging.getLogger(__name__)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        logging_handler = logging.StreamHandler(sys.stdout)
+        logging_handler.setFormatter(formatter)
+        self.log.addHandler(logging_handler)
+        self.log.level = logging.DEBUG
 
-    self.log.info("Scheduling Selinon flow '%s' with node_args: '%s'", 'bayesianFlow', node_args)
-    return run_flow('bayesianFlow', node_args)
+        if ENABLE_SCHEDULING:
+            init_celery(result_backend=False)
+            init_selinon()
 
+    def write_package_info_to_file(self, node_args, file_path):
+        with open(file_path, "a+") as f:
+            f.write(node_args)
+        return True
 
-while True:
-    for i in feed_npm.entries:
-        package_name = i['title']
-        package_url = NPM_URL+"-/package/{package_name}/dist-tags".format(package_name=package_name)
-        package_latest_version = json.loads(requests.get(package_url, headers={'content-type': 'application/json'}).text)
-        print({'package_name': package_name,
-               'latest_version': package_latest_version.get('latest')
-               })
+    def get_correct_ecosystem_filepath(self, ecosystem):
+        if ecosystem == 'npm':
+            return SCHEDULED_NPM_PACKAGES
+        if ecosystem == 'pypi':
+            return SCHEDULED_PYPI_PACKAGES
 
-    for i in feed_pypi.entries:
-        package_name, package_latest_version = i['title'].split(' ')
-        package_url = PYPI_URL+"-/pypi/{package_name}/json".format(package_name=package_name)
-        print({'package_name': package_name,
-               'latest_version': package_latest_version
-               })
+    def run_package_analisys(self, name, ecosystem, version):
+        """Run Selinon flow for analyses.
+         :param name: name of the package to analyse
+         :param version: package version
+         :param ecosystem: package ecosystem
+         :return: dispatcher ID serving flow
+         """
 
+        node_args = {
+            'ecosystem': ecosystem,
+            'name': name,
+            'version': version,
+            'recursive_limit': 0
+        }
 
+        self.write_package_info_to_file(node_args, self.get_correct_ecosystem_filepath(ecosystem))
+        self.log.info("Scheduling Selinon flow '%s' with node_args: '%s'", 'bayesianFlow', node_args)
+        return run_flow('bayesianFlow', node_args)
 
+    def run(self):
+        self.log.info("Initializing RSS feeds")
+        feed_pypi = feedparser.parse(PYPI_URL + "rss/updates.xml")
+        feed_npm = feedparser.parse(NPM_URL + "-/rss")
 
+        while True:
+            for i in feed_npm.entries:
+                package_name = i['title']
+                package_url = NPM_URL + "-/package/{package_name}/dist-tags".format(package_name=package_name)
+                package_latest_version = json.loads(
+                    requests.get(package_url, headers={'content-type': 'application/json'}).text).get('latest')
+                print({'package_name': package_name,
+                       'latest_version': package_latest_version
+                       })
+                self.log.info("Received event for npm: '%s':'%s'", package_name, package_latest_version)
+                if ENABLE_SCHEDULING:
+                    self.run_package_analisys(package_name, 'npm', package_latest_version)
 
-
-
+            for i in feed_pypi.entries:
+                package_name, package_latest_version = i['title'].split(' ')
+                print({'package_name': package_name,
+                       'latest_version': package_latest_version
+                       })
+                self.log.info("Received event for npm: '%s':'%s'", package_name, package_latest_version)
+                if ENABLE_SCHEDULING:
+                    self.run_package_analisys(package_name, 'pypi', package_latest_version)
